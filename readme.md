f9b99126296fc93b7c90e2a702b08b4d441f68ad
Azerus <kevinm@mybestplace.fr>
2020-05-31 17:17:19 +0200

    Profile setup
    Controller and view created, readme updated

--- readme.md
+++ readme.md
@@ -13,3 +13,12 @@ Soon
 ## How to download release 
 
 Just go into the release section and download the lastest available ;)
+
+
+## Status of the project
+
+Still in development at the moment you can setup, and after install acces to a user profile with this route :
+
+localhost/profiles/{username}
+
+Warning a username must be created to get a result.
---
+++ resources/views/profiles/show.blade.php
@@ -0,0 +1,44 @@
+<!-- AzerusGram created by Azerus
+Author : https://www.kmarous.fr
+Mail : developer@mybestplace.fr
+-->
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row mt-4">
+        <div class="col-4 text-center">
+            <img src="https://scontent-mrs2-2.cdninstagram.com/v/t51.2885-19/s150x150/51880691_793452351012640_4481513148892839936_n.jpg?_nc_ht=scontent-mrs2-2.cdninstagram.com&_nc_ohc=eN6txkpUzIYAX8zd9X4&oh=a29be89b87d18a07f09db4d113805af7&oe=5EFE38D8" class="rounded-circle">
+            </div>
+            <div class="col-8">
+                <div class="d-flex">
+                <div class="h4 mr-3 pt-2">Super Devs</div>
+                <button class="btn btn primary">Follow</button>
+                </div>
+                <div class="d-flex mt-3">
+                    <div class="mr-3"><strong>120</strong> posts</div>
+                    <div class="mr-3"><strong>280</strong> followers</div>
+                    <div class="mr-3"><strong>280</strong> following</div>
+                </div>
+                <div class="mt-3">
+                    <div> Developer Work Pics</div>
+                    <div> Are you a code lover?</div>
+                    <a href="#">mybestplace.fr</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-4">
+            <img src="https://scontent-mrs2-1.cdninstagram.com/v/t51.2885-15/e35/101035890_2650908858564752_9124578973153635590_n.jpg?_nc_ht=scontent-mrs2-1.cdninstagram.com&_nc_cat=108&_nc_ohc=5xWvXjiT6OAAX8VdUQx&oh=0e25e5db50a8db0c52b33eb6f0e51ead&oe=5EFC29A3" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-mrs2-2.cdninstagram.com/v/t51.2885-15/e35/101009371_147962220168447_5207563748685445994_n.jpg?_nc_ht=scontent-mrs2-2.cdninstagram.com&_nc_cat=1&_nc_ohc=iomb1xejBDQAX_zrifG&oh=74cfabc4d86fc91ad802f093ece289ca&oe=5EFC4836" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-mrs2-1.cdninstagram.com/v/t51.2885-15/e35/98419103_835600003630632_2190947916549704708_n.jpg?_nc_ht=scontent-mrs2-1.cdninstagram.com&_nc_cat=100&_nc_ohc=vFM5hoStHZUAX9TxU8O&oh=446fb61f2ec449cbb980d9446310a453&oe=5EFD221B" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
