7466b3ccefcbdf8b2b569e94cf590f4ddda73541
Azerus <kevinm@mybestplace.fr>
2020-06-01 15:25:06 +0200

    Profile creation
    Model and controller added, view updated

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -12,8 +12,8 @@ Mail : developer@mybestplace.fr
             <img src="https://scontent-mrs2-2.cdninstagram.com/v/t51.2885-19/s150x150/51880691_793452351012640_4481513148892839936_n.jpg?_nc_ht=scontent-mrs2-2.cdninstagram.com&_nc_ohc=eN6txkpUzIYAX8zd9X4&oh=a29be89b87d18a07f09db4d113805af7&oe=5EFE38D8" class="rounded-circle">
             </div>
             <div class="col-8">
-                <div class="d-flex">
-                <div class="h4 mr-3 pt-2">Super Devs</div>
+                <div class="d-flex align-items-baseline">
+                <div class="h4 mr-3 pt-2">{{ $user->username }}</div>
                 <button class="btn btn primary">Follow</button>
                 </div>
                 <div class="d-flex mt-3">
@@ -22,9 +22,9 @@ Mail : developer@mybestplace.fr
                     <div class="mr-3"><strong>280</strong> following</div>
                 </div>
                 <div class="mt-3">
-                    <div> Developer Work Pics</div>
-                    <div> Are you a code lover?</div>
-                    <a href="#">mybestplace.fr</a>
+                    <div class="font-weight-bold ">{{ $user->profile->title }}</div>
+                    <div> {{ $user->profile->description }}</div>
+                    <a href={{ $user->profile->link }}>{{ $user->profile->link }}</a>
                 </div>
             </div>
         </div>
